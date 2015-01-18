8e4bf1160b859054d1558cb85fbf3f6447c31392
Goth <antekgoth@gmail.com>
2015-01-19 00:32:54 +0100

    Updated db

---
+++ gis-osm/gis-osm-db/liquibase/customNodesRename.sql
@@ -0,0 +1,4 @@
+ alter table segments_set rename to segment_set;
+ alter table custom_nodes rename COLUMN id TO custom_node_id;
+ alter table segments rename COLUMN id TO segment_id;
+ alter table segment_set rename COLUMN id TO segment_set_id;
